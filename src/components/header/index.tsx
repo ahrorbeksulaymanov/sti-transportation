37d2f6f659afa46c3461dc0b12e4ef8876e87382
ahror <ahror@AIFs-iMac.local>
2023-02-17 16:13:46 +0500

    mobile sidebar is done

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -28,11 +28,11 @@ const Header = ({setOpen}:any) => {
                             </Link>
                         </div>
                         <div className="flex justify-end" style={{alignItems:"center"}} >
-                            <p onClick={() => relocate("#about-us")} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>About</p>
-                            <p onClick={() => relocate("#why-sti")} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Why STI</p>
-                            <Link to={'/purchase'} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Lease to purchase</Link>
-                            <Link to={'/blogs'} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Blogs</Link>
-                            <Link to={'/contact'} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Contact</Link>
+                            <p onClick={() => relocate("#about-us")} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] md:block hidden header-menu-effect'>About</p>
+                            <p onClick={() => relocate("#why-sti")} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] md:block hidden header-menu-effect'>Why STI</p>
+                            <Link to={'/purchase'} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] md:block hidden header-menu-effect'>Lease to purchase</Link>
+                            <Link to={'/blogs'} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] md:block hidden header-menu-effect'>Blogs</Link>
+                            <Link to={'/contact'} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] md:block hidden header-menu-effect'>Contact</Link>
                             <a target={'_blank'} href='https://intelliapp.driverapponline.com/c/stitransportationinc?uri_b=ia_stitransportationinc_740871219' className='bg-orange lg:px-[21px] sm:px-[18px] px-[10px] lg:py-[12px] py-[10px] rounded-[100px] lg:text-[16px] sm:text-[14px] text-[12px] hover:bg-orange_hover'>Become a driver</a>
                         </div>
                     </div>
@@ -43,7 +43,7 @@ const Header = ({setOpen}:any) => {
                 <div className="my-container">
                     <div className="flex justify-between" style={{alignItems:"center"}}>
                         <div className="flex justify-start" style={{alignItems:"center"}}>
-                            <svg className='sm:w-[25px] w-[20px] mr-[13px] md:hidden block' fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <svg onClick={() => setOpen(true)} className='sm:w-[25px] w-[20px] mr-[13px] md:hidden block cursor-pointer' fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"></path>
                             </svg>
                             <Link to='/'>
@@ -53,7 +53,7 @@ const Header = ({setOpen}:any) => {
                         <div className="flex justify-end" style={{alignItems:"center"}} >
                             <p onClick={() => relocate("#about-us")} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>About</p>
                             <p onClick={() => relocate("#why-sti")} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Why STI</p>                            
-                            <Link to='/purchase' className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Lease to purchase</Link>
+                            <Link  to='/purchase' className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Lease to purchase</Link>
                             <Link to={'/blogs'} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Blogs</Link>
                             <Link to={'/contact'} className='lg:text-[18px] md:text-[15px] lg:mr-[40px] md:mr-[20px] md:block hidden header-menu-effect'>Contact</Link>
                             <a target={'_blank'} href='https://intelliapp.driverapponline.com/c/stitransportationinc?uri_b=ia_stitransportationinc_740871219' className='bg-orange lg:px-[21px] sm:px-[18px] px-[10px] lg:py-[12px] py-[10px] rounded-[100px] lg:text-[16px] sm:text-[14px] text-[12px] hover:bg-orange_hover'>Become a driver</a>
--- src/components/header/sidebarMobile.tsx
+++ src/components/header/sidebarMobile.tsx
@@ -1,16 +1,28 @@
 import { Drawer } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../../assets/images/logo.png'
 
 const MobileSidebar = ({setOpen, open}:any) => {
   const onClose = () => {
     setOpen(false);
   };
 
+  const navigate = useNavigate()
+    
+  const relocate = async (id:string) => {
+      await navigate("/");
+      window.location.href = id;
+      onClose()
+  }
+
   return (
     <>
-      <Drawer title="Basic Drawer" placement="left" onClose={onClose} open={open}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Drawer title={<><img className='lg:w-[230px] sm:w-[180px] w-[148px] ml-[20px] ' src={Logo} alt="" /></>} placement="left" style={{background:"#000", color:"#fff"}} onClose={onClose} open={open}>
+        <p onClick={() => relocate("#about-us")} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] py-3 cursor-pointer hover:text-[#fff] hover:translate-x-1' style={{transition:"0.2s"}}>About</p>
+        <p onClick={() => relocate("#why-sti")} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] py-3 cursor-pointer hover:text-[#fff] hover:translate-x-1' style={{transition:"0.2s"}}>Why STI</p>
+        <Link onClick={onClose} to={'/purchase'} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] block py-3 cursor-pointer hover:text-[#fff] hover:translate-x-1' style={{transition:"0.2s"}}>Lease to purchase</Link>
+        <Link onClick={onClose} to={'/blogs'} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] block py-3 cursor-pointer hover:text-[#fff] hover:translate-x-1' style={{transition:"0.2s"}}>Blogs</Link>
+        <Link onClick={onClose} to={'/contact'} className='lg:text-[18px] md:text-[15px] xl:mr-[40px] lg:mr-[30px] md:mr-[15px] block py-3 cursor-pointer hover:text-[#fff] hover:translate-x-1' style={{transition:"0.2s"}}>Contact</Link>
       </Drawer>
     </>
   );
