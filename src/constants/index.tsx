7aa0dacad93a4a85d008875131045f36b3477cee
ahror <ahror@AIFs-iMac.local>
2023-02-28 11:05:19 +0500

    base url is changed

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,2 +1,5 @@
-export const PATH_API = 'https://stitransportation.com/api'
-export const PATH_FILE = 'https://stitransportation.com'
+export const PATH_API = 'http://134.209.104.18/api'
+export const PATH_FILE = 'http://134.209.104.18'
+
+// export const PATH_API = 'https://stitransportation.com/api'
+// export const PATH_FILE = 'https://stitransportation.com'
